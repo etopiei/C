5e2cd7de42c60cb45ec5cdd51e6b8f33ed8e1e79
etopiei <lj343@icloud.com>
2017-06-21 17:54:59 +1000

    Worked on bracket matching check.

--- Books/K+R/Chapter1/findUnmatched.c
+++ Books/K+R/Chapter1/findUnmatched.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-#define BRACE_OPEN 0
-#define BRACE_CLOSE 1
-#define BRACKET_OPEN 2
-#define BRACKET_CLOSE 3
-
 int main() {
 
-	int indexOfUnmatchedBrackets[100]; //store an array of unmatched brackets using a stack structure
+	int indexOfUnmatchedBrackets[100]; //store an array of unmatched brackets
 	int indexOfUnmatchedBraces[100]; //same for braces.
 
 	int numBrackets = 0; //add one to this for every opening bracket, minus for closing
@@ -22,25 +17,24 @@ int main() {
 		//if code is any of the bracket types perform operations needed.
 
 		switch (c) {
+
 			case '{':
-				type = BRACE_OPEN;
 				indexOfUnmatchedBraces[numBraces++] = currentIndex;
 				break;
+
 			case '}':
-				type = BRACE_CLOSE;
-				if(numBraces > 0){
-					--numBraces;
-				} //fix this so a closed bracket before an open creates an error.
+			//later add check to see if close brace comes before open
+				--numBraces;
 				break;
+
 			case '(':
-				type = BRACKET_OPEN;
 				indexOfUnmatchedBrackets[numBrackets++] = currentIndex;
 				break;
+
 			case ')':
-				type = BRACKET_CLOSE;
-				--numBrackets;
-				break;
-		}
+			//later add check to see if close bracket comes before open
+					--numBrackets;
+					break;
 
 		currentIndex++;
 
@@ -49,6 +43,7 @@ int main() {
 	if(numBrackets > 0 || numBraces > 0) {
 
 		printf("There are unmatched  brackets or braces in your program.\n");
+		//later put code here to print out locations plus other details about problems.
 
 	} else {
 
